b56a524dc0eb55fda65cb700b85cbe940fd03f6b
Rhesa Utomo <venderblitz@gmail.com>
2018-05-07 15:06:41 +0700

    checkpoint, test csv file output

--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
 "use strict"
+const fs = require('fs');
 
 class Person {
   // Look at the above CSV file
@@ -14,7 +15,6 @@ class Person {
 }
 
 class PersonParser {
-  
   constructor(file) {
     this._file = file
     this._people = this.generateArray();
@@ -30,7 +30,6 @@ class PersonParser {
   }
   
   generateArray() {
-    const fs = require('fs');
     let parser = fs.readFileSync(this._file, 'utf8');
     //console.log(parser);
     let parserArray = parser.split('\n');
@@ -57,28 +56,37 @@ class PersonParser {
       persons.push(array);
     }
     //console.log(persons);
-    let counter = 0
     for (let i = 0; i < persons.length; i++) {
       var person = new Person(persons[i][0], persons[i][1], persons[i][2], persons[i][3], persons[i][4], persons[i][5]);
       this._people.push(person);
-      counter++;
     }
-    // console.log(this._people)
-    // this.size = this._people.length;
-    //console.log(this._people.length);
+    // console.log(this._people);
+    // console.log(this._people.length);
     return this._people
   }
 
-  addPerson(object) {
-
+  addPerson(person) {
+    // push to this._people
+    this._people.push(person);
   }
+
+  save() { // array of objects to string
+    let string = 'id,first_name,last_name,email,phone,created_at\n';
+
+    for (let i = 0; i < this._people.length; i++) {
+      let person = this._people[i];
+      let array = [person.person_id, person.first_name, person.last_name, person.email, person.phone, person.created_at];
+      string = string + array.join(',');
+
+      if (i !== this._people.length - 1) string = string + '\n'
+    }
+    fs.writeFileSync('people2.csv', string);
+  } // override csv
 
 }
 
 let parser = new PersonParser('people.csv')
-// parser.addPerson(new Person(...))
-// parser.save()
+parser.addPerson(new Person('201', 'Rhesa', 'Utomo', 'rhesa.utomo@gmail.com', '081123456789', '2012-05-05T11:07:33-07:00'))
 // console.log(parser.people)
 console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
-
-//console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+parser.save()
