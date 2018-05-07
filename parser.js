e44053d61064a1a5ed6693417ad54b15dd314422
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-07 16:26:06 +0700

    update parser.js

--- parser.js
+++ parser.js
@@ -1,25 +1,80 @@
 "use strict"
 
+const fs = require('fs');
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(id, firstName, lastName, email, phone, createdAt) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.createdAt = createdAt;
+  }
 }
 
 class PersonParser {
-
   constructor(file) {
-    this._file = file
-    this._people = null
-  }
+    this._file = file;
+    this._fileStr = fs.readFileSync(file, 'utf8')
+    this._people = this.backToString();
+  }  
 
   get people() {
-    return this._people
+    return this._people;
   }
 
-  addPerson() {}
+  addPerson() {
+    let arrPeople = [];
+    let row = []
+
+    let lines = this._fileStr.split('\n')
+    let attributes = lines[0]
+    let valueLines = lines.slice(1)
+
+    for(let i = 0; i < valueLines.length; i++){
+      let line = valueLines[i]
+      let lineValues = line.split(",")
+      let id = lineValues[0]
+      let name = lineValues[1];
+      let last = lineValues[2];
+      let email = lineValues[3];
+      let phone = lineValues[4];
+      let createdAt = lineValues[5];
+      var personObj = new Person(id, name, last, email, phone, createdAt);
+      row.push(personObj);
+    }
+
+    return row;
+  }
+
+  backToString() {
+    let input = this.addPerson();
+    var strBack = '';
+    for(let i = 0; i < input.length; i++) {
+      for(let key in input[i]) {
+        if(key == 'createdAt') {
+          strBack += input[i][key];
+        } else {
+          strBack += input[i][key] + ', ';
+        }
+      }
+      strBack += '\n';
+    }
+    return strBack;
+  }
+
+  save() {
+    fs.writeFileSync('newPeople.csv', this.backToString());
+    return 'new data recorded'
+  }
 
 }
 
-let parser = new PersonParser('people.csv')
+let person = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(person.save());
+
+// console.log('There are people in the file ');
+
+// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
